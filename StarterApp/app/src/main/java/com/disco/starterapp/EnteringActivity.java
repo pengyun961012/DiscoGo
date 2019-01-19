79c178cdb306cd9cf62692a3267196e82b133970
lurz <lurz@umich.edu>
2019-01-19 14:59:22 -0500

    cleanup starter

--- StarterApp/app/src/main/java/com/disco/starterapp/EnteringActivity.java
+++ StarterApp/app/src/main/java/com/disco/starterapp/EnteringActivity.java
@@ -1,6 +1,5 @@
 package com.disco.starterapp;
 
-import android.nfc.Tag;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,7 +9,6 @@ import android.content.Intent;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import com.android.volley.Request;
