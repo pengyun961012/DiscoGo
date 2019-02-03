0640339ed6a1844767d041e43d88e652901499fd
lurz <lurz@umich.edu>
2019-02-03 13:29:13 -0500

    audio init

---
+++ AudioProcess/app/src/main/java/com/disco/audioprocess/MainActivity.java
@@ -0,0 +1,100 @@
+package com.disco.audioprocess;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private MediaRecorder myAudioRecorder;
+    private String outputFile;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button recordButton = (Button) findViewById(R.id.recordButton);
+        final Button playButton = (Button) findViewById(R.id.playButton);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    10);
+
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    10);
+
+        }
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+        Log.d("recorder", "onCreate: " + outputFile);
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+        recordButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // Pressed
+                    v.performClick();
+                    try {
+                        myAudioRecorder.prepare();
+                        myAudioRecorder.start();
+                    } catch (IllegalStateException ise) {
+                        // make something ...
+                    } catch (IOException ioe) {
+                        // make something
+                    }
+                    Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_SHORT).show();
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // Released
+                    try {
+                        myAudioRecorder.stop();
+//                        myAudioRecorder.release();
+//                        myAudioRecorder = null;
+                        Toast.makeText(getApplicationContext(), "Audio Recorder stopped", Toast.LENGTH_SHORT).show();
+                    } catch (RuntimeException stopException) {
+                        Log.d("recorder", "onTouch: " + stopException.toString());
+                    }
+                }
+                return false;
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                try {
+                    mediaPlayer.setDataSource(outputFile);
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+                    Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    // make something
+                }
+            }
+        });
+
+    }
+
+}
