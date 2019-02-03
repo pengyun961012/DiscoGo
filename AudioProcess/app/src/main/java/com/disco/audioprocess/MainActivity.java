6c6b106f502ce94165c17c2c2e65266375ccc167
lurz <lurz@umich.edu>
2019-02-03 17:49:41 -0500

    audio visualize

--- AudioProcess/app/src/main/java/com/disco/audioprocess/MainActivity.java
+++ AudioProcess/app/src/main/java/com/disco/audioprocess/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Environment;
+import android.os.Handler;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,13 +19,24 @@ import android.widget.Toast;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    private MediaRecorder myAudioRecorder;
+    private VisualizerView visualizerView;
+    private MediaRecorder myAudioRecorder = new MediaRecorder();
     private String outputFile;
     private Button facebookButton;
     private Button googleButton;
     private Button recordButton;
     private Button playButton;
 
+    private Handler handler = new Handler();
+    final Runnable updater = new Runnable() {
+        public void run() {
+            handler.postDelayed(this, 1);
+            int maxAmplitude = myAudioRecorder.getMaxAmplitude();
+            if (maxAmplitude != 0) {
+                visualizerView.addAmplitude(maxAmplitude);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,59 +60,65 @@ public class MainActivity extends AppCompatActivity {
                     10);
 
         }
+
         outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
-        Log.d("recorder", "onCreate: " + outputFile);
-        myAudioRecorder = new MediaRecorder();
-        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        myAudioRecorder.setOutputFile(outputFile);
+        visualizerView = (VisualizerView) findViewById(R.id.visualizer);
+        try {
+            myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            myAudioRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            myAudioRecorder.setOutputFile(outputFile);
+            myAudioRecorder.prepare();
+            myAudioRecorder.start();
+        } catch (IllegalStateException | IOException ignored) {
 
-        recordButton.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                    // Pressed
-                    v.performClick();
-                    try {
-                        myAudioRecorder.prepare();
-                        myAudioRecorder.start();
-                    } catch (IllegalStateException ise) {
-                        // make something ...
-                    } catch (IOException ioe) {
-                        // make something
-                    }
-                    Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_SHORT).show();
-
-                } else if (event.getAction() == MotionEvent.ACTION_UP) {
-                    // Released
-                    try {
-                        myAudioRecorder.stop();
-//                        myAudioRecorder.release();
-//                        myAudioRecorder = null;
-                        Toast.makeText(getApplicationContext(), "Audio Recorder stopped", Toast.LENGTH_SHORT).show();
-                    } catch (RuntimeException stopException) {
-                        Log.d("recorder", "onTouch: " + stopException.toString());
-                    }
-                }
-                return false;
-            }
-        });
-
-        playButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                try {
-                    mediaPlayer.setDataSource(outputFile);
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                    Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    // make something
-                }
-            }
-        });
+        }
+//
+//        recordButton.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+//                    // Pressed
+//                    v.performClick();
+//                    try {
+//                        myAudioRecorder.prepare();
+//                        myAudioRecorder.start();
+//                    } catch (IllegalStateException ise) {
+//                        // make something ...
+//                    } catch (IOException ioe) {
+//                        // make something
+//                    }
+//                    Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_SHORT).show();
+//
+//                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+//                    // Released
+//                    try {
+//                        myAudioRecorder.stop();
+////                        myAudioRecorder.release();
+////                        myAudioRecorder = null;
+//                        Toast.makeText(getApplicationContext(), "Audio Recorder stopped", Toast.LENGTH_SHORT).show();
+//                    } catch (RuntimeException stopException) {
+//                        Log.d("recorder", "onTouch: " + stopException.toString());
+//                    }
+//                }
+//                return false;
+//            }
+//        });
+//
+//        playButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MediaPlayer mediaPlayer = new MediaPlayer();
+//                try {
+//                    mediaPlayer.setDataSource(outputFile);
+//                    mediaPlayer.prepare();
+//                    mediaPlayer.start();
+//                    Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_SHORT).show();
+//                } catch (Exception e) {
+//                    // make something
+//                }
+//            }
+//        });
 
         facebookButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -117,6 +135,22 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        handler.removeCallbacks(updater);
+        myAudioRecorder.stop();
+        myAudioRecorder.reset();
+        myAudioRecorder.release();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        handler.post(updater);
     }
 
 }
