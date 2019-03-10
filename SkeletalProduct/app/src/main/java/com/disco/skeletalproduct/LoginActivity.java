9635b52dbaa54886903740612967160b2695fe38
lurz <lurz@umich.edu>
2019-03-09 23:08:15 -0500

    beautiful layout

--- SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/LoginActivity.java
+++ SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/LoginActivity.java
@@ -3,8 +3,10 @@ package com.disco.skeletalproduct;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
@@ -63,6 +65,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private FloatingActionButton backButton;
+    private FloatingActionButton homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +74,24 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+        backButton = (FloatingActionButton) findViewById(R.id.backfloatingActionButton);
+        homeButton = (FloatingActionButton) findViewById(R.id.homefloatingActionButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
--- SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/ShopActivity.java
+++ SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/ShopActivity.java
@@ -1,11 +1,17 @@
 package com.disco.skeletalproduct;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class ShopActivity extends AppCompatActivity {
+
+    private FloatingActionButton backButton;
+    private FloatingActionButton homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,23 @@ public class ShopActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_shop);
+
+        backButton = (FloatingActionButton) findViewById(R.id.backfloatingActionButton);
+        homeButton = (FloatingActionButton) findViewById(R.id.homefloatingActionButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ShopActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
