7532dfb8e9f7ace8d4325f95e0ae73d0deee9ebd
lurz <lurz@umich.edu>
2019-03-09 23:08:15 -0500

    beautiful layout

--- SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/PlayActivity.java
+++ SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/PlayActivity.java
@@ -1,6 +1,7 @@
 package com.disco.skeletalproduct;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -23,6 +24,8 @@ import be.tarsos.dsp.pitch.PitchProcessor;
 public class PlayActivity extends AppCompatActivity {
 
     private Button goUnityButton;
+    private FloatingActionButton backButton;
+    private FloatingActionButton homeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,8 @@ public class PlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_play);
 
         goUnityButton = (Button) findViewById(R.id.go_unity_button);
+        backButton = (FloatingActionButton) findViewById(R.id.backfloatingActionButton);
+        homeButton = (FloatingActionButton) findViewById(R.id.homefloatingActionButton);
 
         goUnityButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,6 +49,21 @@ public class PlayActivity extends AppCompatActivity {
                 Intent intent = new Intent(PlayActivity.this,  UnityPlayerActivity.class);
                 startActivity(intent);
                 
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayActivity.this, MainActivity.class);
+                startActivity(intent);
             }
         });
     }
--- SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/SplashScreenActivity.java
+++ SkeletalProduct/app/src/main/java/com/disco/skeletalproduct/SplashScreenActivity.java
@@ -2,17 +2,31 @@ package com.disco.skeletalproduct;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+        new CountDownTimer(2000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            public void onFinish() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }.start();
     }
 
 }
